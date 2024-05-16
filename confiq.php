81f30eadfd831e8ce1fdadd52f279c3e482a3fa3
muraakib <muraakib@gmail.com>
2024-05-16 18:28:57 +0600

    Back-end-updating

---
+++ confiq.php
@@ -0,0 +1,14 @@
+<?php
+    $db_name='admin3';
+    $db_user='root';
+    $db_pass='';
+    $db_host='localhost';
+
+    $con=mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+
+    if(!$con){
+
+        echo "Database Connection Error.";
+    }
+?>
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+    // require('includes/header.php');
+    // require_once('includes/header.php');
+    // require_once('includes/sidebar.php');
+    // include_once('includes/header.php');
+
+
+    require_once('functions/function.php');
+    get_header();
+    get_sidebar();
+?>
+    
+    <div class="row">
+        <div class="col-md-12 welcome_part">
+            <p><span>Welcome Mr.</span> Muhammad Rakib</p>
+        </div>
+    </div>
+
+
+<?php
+    get_footer();
+?>
+             
